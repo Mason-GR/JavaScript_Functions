294fbce41a880293fb1126a34b28e8c5f3813d14
Mason-GR <mase210@gmail.com>
2021-07-26 10:42:52 -0400

    Completed Exer. 1 & Bonus, Exer. 2 & Practice 1

--- app.js
+++ app.js
@@ -20,13 +20,14 @@ printOdds(-100);
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 function checkAge(age, userName) {
-    // if (age = 0) {
-    //     console.log("Please input your age and name.")
-    // } else if (userName = " ") {
-    //     console.log(`${userName} is not a valid name please try again.`)
-    // } 
+  // if (age = 0) {
+  //     console.log("Please input your age and name.")
+  // } else if (userName = " ") {
+  //     console.log(`${userName} is not a valid name please try again.`)
+  // }
   let aboveSixteen = `Congrats ${userName}, you can drive!`;
   let belowSixteen = `Sorry ${userName}, but you need to wait until you're 16.`;
+  // let noUserName = "Please input a valid name to continue.";
   if (age >= 16) {
     console.log(aboveSixteen);
   } else {
@@ -35,3 +36,21 @@ function checkAge(age, userName) {
 }
 
 checkAge(30, "Mason Gregory");
+
+//Assign a function to a variable
+let printEvens = function (countUp) {
+  for (let num = 1; num < countUp; num++) {
+    if (num % 2 == 0) {
+      console.log(num);
+    } else {
+      continue;
+    }
+  }
+};
+
+printEvens(100)
+
+//Pass a function as a parameter(argument)
+
+
+//Return a function from a function
